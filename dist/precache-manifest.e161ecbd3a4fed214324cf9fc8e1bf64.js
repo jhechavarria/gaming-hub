9d96f87adab64b99265c22aab01a669bd717f629
jhechavarria <jessie.hechavarria@gmail.com>
2019-08-28 23:36:58 +0200

    test

---
+++ dist/precache-manifest.e161ecbd3a4fed214324cf9fc8e1bf64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ef53dfb7c7d578bbc4",
+    "url": "/dist/js/about.621f13f7.js"
+  },
+  {
+    "revision": "63399b8babe9bff9fa2b",
+    "url": "/dist/css/app.99dd9837.css"
+  },
+  {
+    "revision": "63399b8babe9bff9fa2b",
+    "url": "/dist/js/app.d64f3df4.js"
+  },
+  {
+    "revision": "fc2c5c5fa94dc1784419",
+    "url": "/dist/js/chunk-3e813ae3.e1787b4d.js"
+  },
+  {
+    "revision": "a6432659d7d1e2e05f3b",
+    "url": "/dist/css/chunk-vendors.3aeb0ed2.css"
+  },
+  {
+    "revision": "a6432659d7d1e2e05f3b",
+    "url": "/dist/js/chunk-vendors.bb145168.js"
+  },
+  {
+    "revision": "f66ece901c1953a012d50fcff521537d",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  }
+];
