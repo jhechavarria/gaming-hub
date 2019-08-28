4f7660571a76870a11b5aafc505cbe6f1a43d4b9
jhechavarria <jessie.hechavarria@gmail.com>
2019-08-29 00:54:17 +0200

    ok

--- js/about.621f13f7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,a){"use strict";a.r(n);var e=function(){var t=this,n=t.$createElement,a=t._self._c||n;return a("div",{staticClass:"about"},[a("h1",[t._v("This is an about page")]),a("v-btn",{attrs:{to:"/"}},[t._v("Home")])],1)},s=[],o=a("2877"),u=a("6544"),i=a.n(u),l=a("8336"),c={},r=Object(o["a"])(c,e,s,!1,null,null,null);n["default"]=r.exports;i()(r,{VBtn:l["a"]})}}]);
-//# sourceMappingURL=about.621f13f7.js.map
